ba757d8531dba22f8daebda420b65af424780105
Nur Rachmat <rachmat.nur91@gmail.com>
2021-11-22 09:33:37 +0700

    Belajar VUE Router
    Router view dan router link

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
